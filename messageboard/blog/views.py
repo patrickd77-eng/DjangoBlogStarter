c5d586d1211b27c8fa31784056a0a35a91b6a9b5
Patrick Davis <patrick_davis97@hotmail.co.uk>
2019-09-22 20:50:56 +0100

    added base template etc

--- messageboard/blog/views.py
+++ messageboard/blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author': 'Patrick Davis',
+        'title': 'One',
+        'content': 'Lorem Ipsum.',
+        'date_posted': "22 Sep 19"
+    },
+    {   'author': 'Patrick Davis',
+        'title': 'Two',
+        'content': 'Lorem Ipsum Two.',
+        'date_posted': "21 Sep 19"
+    },
+]
+
+def home(request):
+    context= {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    context={
+        'title': 'About'
+    }
+    return render(request, 'blog/about.html', context)
