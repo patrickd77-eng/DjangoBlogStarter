ffdef5b33c556f8822bf747cceffae5d73ba51af
Patrick Davis <patrick_davis97@hotmail.co.uk>
2019-09-22 21:20:41 +0100

    site framework and styling established

--- messageboard/blog/urls.py
+++ messageboard/blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='board-home'),
-    path('about/', views.about, name='blog-about'),
+    path('about/', views.about, name='board-about'),
 ]
