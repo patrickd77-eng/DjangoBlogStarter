6040e448853747310603d8ad7fa92ad24fffc6b7
Patrick Davis <patrick_davis97@hotmail.co.uk>
2019-09-24 13:17:42 +0100

    blogs rendered properly as list

--- messageboard/blog/urls.py
+++ messageboard/blog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import PostListView
 
 urlpatterns = [
-    path('', views.home, name='board-home'),
+    path('', PostListView.as_view(), name='board-home'),
     path('about/', views.about, name='board-about'),
-    path('admin/', views.about, name='board-admin'),
+    path('admin/', views.home, name='board-admin'),
 ]
--- messageboard/blog/views.py
+++ messageboard/blog/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView
 from .models import Post
 
 
 def home(request):
-    context= {
+    context = {
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
 def about(request):
-    context={
+    context = {
         'title': 'About'
     }
     return render(request, 'blog/about.html', context)
+
