3204c9d9df17e5a69be869bcf656b9a6bb49519b
Patrick Davis <patrick_davis97@hotmail.co.uk>
2019-09-24 22:35:34 +0100

    Removed register option for security.

--- messageboard/messageboard/urls.py
+++ messageboard/messageboard/urls.py
@@ -9,7 +9,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
