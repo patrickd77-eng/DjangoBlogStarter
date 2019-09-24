0314062cce5fbae1f2c79babc2801c8cab04a9a8
Patrick Davis <patrick_davis97@hotmail.co.uk>
2019-09-24 15:20:54 +0100

    Added full CRUD functionality. Also restructured

--- messageboard/blog/urls.py
+++ messageboard/blog/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import PostListView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='board-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='board-about'),
     path('admin/', views.home, name='board-admin'),
 ]
--- messageboard/blog/views.py
+++ messageboard/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from datetime import datetime
+
 
 
 def home(request):
@@ -15,9 +18,45 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
+class PostDetailView(DetailView):
+    model = Post
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 def about(request):
     context = {
-        'title': 'About'
+        'title': 'About',
+        'now': datetime.utcnow()
     }
     return render(request, 'blog/about.html', context)
 
